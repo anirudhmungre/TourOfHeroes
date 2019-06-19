2d4e79336177c670e7accc42980ac9086ffd8d45
Anirudh Mungre <anirudh.mungre@uoit.net>
2019-06-19 10:53:48 -0400

    sleected and mock heroes update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../Objects/hero';
+import { Hero } from '../objects/hero';
+import { HEROES } from '../data/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass']
+  styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
