2d5e84a342b75ebce66b6ab07a667b6a8c2cb293
Anirudh Mungre <anirudh.mungre@uoit.net>
2019-06-19 12:02:24 -0400

    service to retrieve heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './objects/hero';
+import { HEROES } from './data/mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HeroService {
 
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
   constructor() { }
 }
